17a4e82b33e63d022a020f19d34422e2466330a2
van-dt <dangvan31102001@gmail.com>
2024-12-28 18:43:35 +0700

    update cd

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,23 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // Auto re-write OpenAPI local document to file for az api management
+  const localDocument = {
+    ...document,
+    servers: [
+      {
+        description: `Testing API`,
+      },
+    ],
+  };
+
+  try {
+    fs.writeFileSync('./openapi.json', JSON.stringify(localDocument, null, 2));
+    console.info(`OpenAPI document written to openapi.json`);
+  } catch (error) {
+    console.info('Error writing OpenAPI document to file', error);
+  }
 
   await app.listen(3000);
 }
